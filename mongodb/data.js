d00e1dd1ee672b5b4689d232e458967cc80cc02d
Ubuntu <ubuntu@ip-172-31-62-197.ec2.internal>
2023-08-23 12:20:38 +0000

    g

--- mongodb/data.js
+++ mongodb/data.js
@@ -1,5 +1,5 @@
 db.apples.insertMany([
-  { "_id": 1, "name": "apples", "qty": 5, "rating": 3 },
+  { "_id": 1, "name": "apples", "qty": 10, "rating": 3 },
   { "_id": 2, "name": "bananas", "qty": 7, "rating": 1 },
   { "_id": 3, "name": "oranges", "qty": 6, "rating": 2 },
   { "_id": 4, "name": "avocados", "qty": 3, "rating": 5 }
